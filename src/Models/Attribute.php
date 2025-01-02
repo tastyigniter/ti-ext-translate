aad2d7a47414fff9b50efbf24d30f7749303bf66
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-01-02 23:47:53 +0000

    chore: add property annotations to models
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Models/Attribute.php
+++ src/Models/Attribute.php
@@ -6,6 +6,13 @@ use Igniter\Flame\Database\Model;
 
 /**
  * Attribute Model
+ *
+ * @property int $id
+ * @property string $locale
+ * @property string|null $translatable_id
+ * @property string|null $translatable_type
+ * @property string|null $attribute
+ * @mixin \Igniter\Flame\Database\Model
  */
 class Attribute extends Model
 {
